84c9a660f4b56c621ae08199e627b72f18a3fc08
Adam <adamturner98@hotmail.com>
2018-09-19 19:56:55 +1000

    terrain

---
+++ Assets/movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public int rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += this.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position -= this.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.down * Time.deltaTime * rotateSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+        }
+    }
+}
