fc020651040c83e68f86a9c6746a8b821c89f3cd
Shevon Mendis <shevonmendis98@gmail.com>
2018-09-26 18:34:26 +1000

    partially working lasers

---
+++ Assets/Laser/LaserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour {
+
+    public Vector3 direction;
+    private float velocity = 1.0f;
+
+    void Update () {
+        this.transform.Translate(direction * Time.deltaTime * velocity);
+
+        // dummy code to destroy laser objects
+        if (this.transform.position.y < -12){
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //if (col.gameObject.tag == tagToDamage)
+        //{
+        //    // Damage object with relevant tag
+        //    HealthManager healthManager = col.gameObject.GetComponent<HealthManager>();
+        //    healthManager.ApplyDamage(damageAmount);
+
+        //    // Destroy self
+        //    Destroy(this.gameObject);
+        //}
+    }
+}
--- Assets/Seagull/SeagullController.cs
+++ Assets/Seagull/SeagullController.cs
@@ -7,17 +7,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeagullController : MonoBehaviour {
+public class SeagullController : MonoBehaviour
+{
 
+    public GameObject laserPrefab;
+
+    private GameObject target;
+    private GameObject laser;
     private const float orbitSpeed = 1.5f;
     private const float heightChangeSpeed = 0.5f;
     private const float radiusOfOrbit = 120.0f;
     private const float height = 125.0f;
     private const float maxHeightChange = 25.0f;
     private const float bankAngle = -20.0f;
+    private float fireFrequency = 2;
+    //private const float frequencyStep = 0.25f;
+    private Vector3[] laserPositions = new Vector3[] { new Vector3(3.0f, 34.5f, 20.0f), new Vector3(-3.0f, 34.5f, 20.0f) };
 
     private float totalTime = 0;
     private float delta;
+    private float timeSinceFire = 0;
 
     void Start()
     {
@@ -28,19 +37,37 @@ public class SeagullController : MonoBehaviour {
         // bank towards the arena
         this.transform.Rotate(Vector3.right, bankAngle, Space.Self); //NEEDS TO BE FIXED
 
+        // identify squidward as targer
+        target = GameObject.Find("squidward");
     }
 
-    void Update () {
+    void Update()
+    {
 
         delta = Time.deltaTime;
         totalTime += delta;
+        timeSinceFire += delta;
 
         // make the seagull move in an orbit over the arena
         this.transform.position = new Vector3(radiusOfOrbit * Mathf.Sin(totalTime * orbitSpeed), height, radiusOfOrbit * Mathf.Cos(totalTime * orbitSpeed));
         this.transform.position += new Vector3(0.0f, maxHeightChange * Mathf.Sin(totalTime * heightChangeSpeed), 0.0f);
 
         // orient the seagull to face the right direction
-        this.transform.Rotate(Vector3.up, (delta/ (Mathf.PI *2)) * 360 * orbitSpeed, Space.Self);
+        this.transform.Rotate(Vector3.up, (delta / (Mathf.PI * 2)) * 360 * orbitSpeed, Space.Self);
 
+        if (timeSinceFire >= fireFrequency)
+        {
+            timeSinceFire = 0;
+
+            Debug.Log("Squidward's position: " + target.transform.position);
+
+            for (int i = 0; i < laserPositions.Length; i++)
+            {
+                Vector3 laserPos = this.transform.position + laserPositions[i];
+                Vector3 direction = target.transform.position - laserPos;
+                laser = Instantiate<GameObject>(laserPrefab, laserPos, Quaternion.LookRotation(new Vector3(0,0,0)));
+                laser.GetComponent<LaserController>().direction = direction;
+            }
+        }
     }
 }
