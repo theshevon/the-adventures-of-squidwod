487275ceba9ce883999be67b9f4ab9a6ad7237aa
aturnerr <adamturner98@hotmail.com>
2018-10-01 20:08:11 +1000

    resolved conflicts

---
+++ Assets/Squidward/cameraControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour
+{
+	public bool lockCursor;
+	public float mouseSensitivity = 10;
+	public Transform cameraTarget;
+	public float distanceFromTarget = 20;
+	public Vector2 pitchMinMax = new Vector2(-10, 70);
+
+	public float rotationSmoothTime = 0.1f;
+	private Vector3 rotationSmoothVelocity;
+	private Vector3 currentRotation;
+	
+	private float yaw;
+	private float pitch;
+	
+	// Use this for initialization
+	void Start () {
+		if (lockCursor)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+		currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity,
+			rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+		transform.position = cameraTarget.position - transform.forward * distanceFromTarget;
+	}
+
+}
--- Assets/Squidward/movement.cs
+++ Assets/Squidward/movement.cs
@@ -2,33 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour {
+public class movement : MonoBehaviour
+{
 
+	public float speed = 3;
+	
     public int rotateSpeed;
+    private Transform cameraT;
 
+	public float turnSmoothTime = 0.2f;
+	private float turnSmoothVelocity;
+	
+	public float speedSmoothTime = 0.1f;
+	private float speedSmoothVelocity;
+	private float currentSpeed;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    cameraT = Camera.main.transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            this.transform.position += this.transform.forward;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            this.transform.position -= this.transform.forward;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            this.transform.Rotate(Vector3.down * Time.deltaTime * rotateSpeed);
-        }
+	void Update ()
+	{
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		Vector2 inputDir = input.normalized;
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            this.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
-        }
-    }
+		if (inputDir != Vector2.zero)
+		{
+			float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation,
+				                        ref turnSmoothVelocity, turnSmoothTime);
+		}
+
+		float targetSpeed = speed * inputDir.magnitude;
+		currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+		
+		transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+		
+	}
 }
