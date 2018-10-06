6a72b5a974c1161409805af646d56b9737dd9e9d
aturnerr <adamturner98@hotmail.com>
2018-10-06 13:22:49 +1000

    seagull laser beam

--- Assets/Seagull/SeagullController.cs
+++ Assets/Seagull/SeagullController.cs
@@ -21,6 +21,17 @@ public class SeagullController : MonoBehaviour
     const float ySpeed = 0.5f;
     const float maxHeightChange = 25.0f;
     const float bankAngle = -20.0f;
+
+    [Range(1,5)]
+    public int difficulty = 1;
+    public Material laserMaterial;
+    private Vector3 laserTarget;
+    private Vector3 laserDirection;
+    public float laserMoveLength = 80f;
+    private float startTime;
+
+    private LineRenderer laser;
+    
     //const float frequencyStepTime = 10f;
     //const float frequencyStep = 0.25f;
 
@@ -37,6 +48,11 @@ public class SeagullController : MonoBehaviour
 
         countdown = fireDelay;
         audioSrc.clip = clip;
+        
+        laser = gameObject.AddComponent<LineRenderer>();
+        laser.material = laserMaterial;
+        laser.endWidth = 0.4f;
+        laser.startWidth = 0.4f;
     }
 
     void Update()
@@ -55,29 +71,78 @@ public class SeagullController : MonoBehaviour
         this.transform.Rotate(Vector3.up, delta / (Mathf.PI * 2) * 360 * orbitSpeed, Space.Self);
 
         // fire lasers from the seagull's eyes
+        if (difficulty == 1) { Level1(); }
+        if (difficulty == 2) { Level2(); }
+        
+
+    }
+
+    void Level1()
+    {
         if (countdown <= 0)
         {
-            audioSrc.Play();
-
             countdown = fireDelay;
-
-            // left eye laser
-            Vector3 direction = target.transform.position - leftEye.position;
-            GameObject laser = Instantiate(laserPrefab, leftEye.position, Quaternion.LookRotation(direction));
-            //laser.transform.position = leftEye.position;
-            //laser.transform.LookAt(Vector3.zero);
-            laser.GetComponent<LaserController>().direction = direction;
-
-            //Debug.DrawLine(Vector3.zero, -direction);
-
-            // right eye laser
-            direction = target.transform.position - rightEye.position;
-            laser = Instantiate(laserPrefab, rightEye.position, Quaternion.LookRotation(direction));
-            //laser.transform.position = rightEye.position;
-            //laser.transform.LookAt(Vector3.zero);
-            laser.GetComponent<LaserController>().direction = direction;
-
-            //Debug.DrawLine(Vector3.zero, -direction);
+            ShootLaser();
         }
+    }
+
+    void Level2()
+    {
+        if (countdown >= 0)
+        {
+            laserTarget = target.transform.position + target.transform.forward*30;
+            laserTarget.y = 0;
+            laserDirection = Random.insideUnitCircle;
+            laserDirection.z = laserDirection.y;
+            laserDirection.y = 0;
+            startTime = Time.time;
+        }
+        if ((countdown >= -2) & (countdown <= 0))
+        {
+            ShootLongLaser();
+        }
+        if ((countdown <= -2))
+        {
+            laser.positionCount = 0;
+            countdown = fireDelay;
+            ShootLaser();
+        }
+    }
+    
+
+    void ShootLaser()
+    {
+        audioSrc.Play();
+        // left eye laser
+        Vector3 direction = target.transform.position - leftEye.position;
+        GameObject laser = Instantiate(laserPrefab, leftEye.position, Quaternion.LookRotation(direction));
+        //laser.transform.position = leftEye.position;
+        //laser.transform.LookAt(Vector3.zero);
+        laser.GetComponent<LaserController>().direction = direction;
+    
+        //Debug.DrawLine(Vector3.zero, -direction);
+    
+        // right eye laser
+        direction = target.transform.position - rightEye.position;
+        laser = Instantiate(laserPrefab, rightEye.position, Quaternion.LookRotation(direction));
+        //laser.transform.position = rightEye.position;
+        //laser.transform.LookAt(Vector3.zero);
+        laser.GetComponent<LaserController>().direction = direction;
+    
+        //Debug.DrawLine(Vector3.zero, -direction);
+    }
+
+    void ShootLongLaser()
+    {
+        laserDirection.Normalize();
+        float length = Vector3.Distance(laserTarget, laserDirection * laserMoveLength);
+        float disCovered = (Time.time - startTime) * 25;
+        Vector3 laserPosition = Vector3.Lerp(laserTarget, laserDirection * laserMoveLength, disCovered / length);
+        laser.positionCount = 4;
+        //laser.SetPosition(0, laserPos + (laserDirection * Time.deltaTime * 10));
+        laser.SetPosition(0, laserPosition);
+        laser.SetPosition(1, rightEye.position);
+        laser.SetPosition(2, leftEye.position);
+        laser.SetPosition(3, laserPosition);
     }
 }
