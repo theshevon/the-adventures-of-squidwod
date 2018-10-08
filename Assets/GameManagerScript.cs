385705e94786dbbff5a89d39b206203e708749af
Shevon Mendis <shevonmendis98@gmail.com>
2018-10-09 02:09:16 +1100

    Integrated game manager + bird turn to face

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour {
 
-    //NEEDS TO INCLUDE CRAB AND EGG SPAWNER
 
     public GameObject player;
     public GameObject enemy;
     public GameObject Crab;
     public GameObject CrabBurrow;
+    public GameObject EggPrefab;
     public Vector3 centre;
     public int diameter;
 
+    const float eggHeight = 7.37f;
+    const float crabHeight = 2;
     bool inBossFight;
     float countdown = 5f;
 
     void Start()
     {
-        //SpawnCrab(new Vector3(0,transform.position.y,0));
+        if (!inBossFight){
+            SpawnEgg(new Vector3(0, eggHeight, 0));
+        }
     }
 
     void Update () {
+
         countdown -= Time.deltaTime;
         if (countdown <= 0)
         {
@@ -33,16 +38,16 @@ public class GameManagerScript : MonoBehaviour {
     // spawn a crab in a random location
     public void SpawnCrab()
     {
-        Vector3 pos = centre + new Vector3(Random.Range(-diameter / 2, diameter / 2), transform.position.y, Random.Range(-diameter / 2, diameter / 2));
+        Vector3 pos = centre + new Vector3(Random.Range(-diameter / 2, diameter / 2), crabHeight, Random.Range(-diameter / 2, diameter / 2));
 
-        Instantiate(CrabBurrow, pos, transform.rotation);
+        Instantiate(CrabBurrow, pos + new Vector3(0,crabHeight,0), transform.rotation);
         StartCoroutine(WaitToSpawn(pos));
 
     }
     // spawn a crab in a specific location
     public void SpawnCrab(Vector3 pos)
     {
-        Instantiate(CrabBurrow, pos, transform.rotation);
+        Instantiate(CrabBurrow, pos + new Vector3(0, crabHeight, 0), transform.rotation);
         StartCoroutine(WaitToSpawn(pos));
     }
 
@@ -51,4 +56,33 @@ public class GameManagerScript : MonoBehaviour {
         yield return new WaitForSeconds(5f);
         Instantiate(Crab, pos, Crab.transform.rotation);
     }
+
+    // spawn an egg in a random location
+    public void SpawnEgg()
+    {
+        Vector3 pos = centre + new Vector3(Random.Range(-diameter / 2, diameter / 2), eggHeight, Random.Range(-diameter / 2, diameter / 2));
+        Instantiate(EggPrefab, pos, Quaternion.identity);
+    }
+    // spawn an egg in a specific location
+    public void SpawnEgg(Vector3 pos)
+    {
+        Instantiate(EggPrefab, pos, Quaternion.identity);
+    }
+
+    /*public void CollectEgg()
+    {
+        spinSpeed = Mathf.Pow(spinSpeed, 2.0f);
+        this.transform.position += transform.up * Time.deltaTime;
+    }*/
+    /*
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Egg has collided!");
+        if (col.gameObject.tag == "Player")
+        {
+            SpawnEgg();
+            Destroy(gameObject);
+
+        }
+    }*/
 }
