5dca097610db55e26614afd11b238aabbf4e1e41
Shevon Mendis <shevonmendis98@gmail.com>
2018-10-17 16:53:45 +1100

    working menu

---
+++ Assets/TutorialSlidesScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialSlidesScript : MonoBehaviour {
+
+    public Sprite[] images;
+    int index = 0;
+	
+
+	void Update () {
+        if (Input.anyKeyDown){
+            index += 1;
+            if (index == images.Length){
+                index = 0;
+            }
+            gameObject.GetComponent<Image>().sprite = images[index];
+        }
+	}
+}
